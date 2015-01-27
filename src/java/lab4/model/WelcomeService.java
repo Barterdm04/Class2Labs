8a76e431404beba4668764327ffa9eba9bb91097
Dylan <Dylan@Dylan-Office.attlocal.net>
2015-01-26 22:36:36 -0600

    finalized

---
+++ src/java/lab4/model/WelcomeService.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+/**
+ *
+ * @author dbarter1
+ */
+public class WelcomeService {
+    private int morningEndTime = 12;
+    private int afternoonEndTime = 20;
+    private int eveningEndTime = 4;
+    private final LocalDateTime currentDate = LocalDateTime.now();
+    
+    
+    public String determineTimeOfDay(){
+        int currentHour = currentDate.getHour();
+        int morningEndTime = 12;
+        int afternoonEndTime = 20;
+        int eveningEndTime = 4;
+        
+        System.out.println(currentDate);
+        if(currentHour < morningEndTime && currentHour >= eveningEndTime) {
+            return "Morning";
+        } else if (currentDate.getHour() < afternoonEndTime){
+            return "Afternoon";
+        } else if (currentDate.getHour() > afternoonEndTime || currentDate.getHour() < eveningEndTime){
+            return "Evening";
+        } else {
+            return " ERROR ";
+        }
+    }
+        
+    public String createMessage(String name){
+        return "Good " + determineTimeOfDay() + ", " + name + ". Welcome!";
+    }
+    
+    
+}
